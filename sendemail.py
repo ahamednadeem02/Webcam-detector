4c0ab3d12a16b2235dac3b6f204c043c655aaa12
ahamednadeem02 <111524475+ahamednadeem02@users.noreply.github.com>
2023-04-24 22:18:34 +0530

    created main and sendemail

---
+++ sendemail.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+SENDER = "ahamednadeem02@gmail.com"
+PASSWORD = "Type your application password here"
+RECEIVER = "ahamednadeem02@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message['Subject'] = "New customer showed up"
+    email_message.set_content("Hey, we just saw a new customer")
+
+    with open(image_path, 'rb') as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="Your image path to test the code")
+
+
+
+
+
+
