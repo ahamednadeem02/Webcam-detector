73282fca34fb134aac774b859d3c3542c35fb87c
ahamednadeem02 <111524475+ahamednadeem02@users.noreply.github.com>
2023-04-24 22:41:46 +0530

    final commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import cv2
 import time
 from sendemail import send_email
 import glob
+import os
+from threading import Thread
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -9,6 +12,12 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 0
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 while True:
     status = 0
     check, frame = video.read()
@@ -40,14 +49,20 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
+        email.thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean.thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+
 
     cv2.imshow("Video", frame)
-
     key = cv2.waitKey(1)
 
     if key == ord("q"):
         break
 
 video.release()
+clean_thread.start()
 
